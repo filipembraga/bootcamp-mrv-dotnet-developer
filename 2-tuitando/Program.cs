5b56ba169624ded690ef66b92825122426dc0a81
Filipe Braga <lipebraga16@hotmail.com>
2021-04-29 19:09:09 -0300

    Resolvendo Desafio 2

---
+++ 2-tuitando/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _2_tuitando
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string arr = Console.ReadLine();
+            if(arr.Length <= 140)
+                Console.WriteLine("TWEET");
+            else
+                Console.WriteLine("MUTE");
+        }
+    }
+}
