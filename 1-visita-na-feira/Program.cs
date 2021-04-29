a7aacf349f5344ba17420e7d4491661151abf08d
Filipe Braga <lipebraga16@hotmail.com>
2021-04-29 18:15:58 -0300

    Resolvendo Desafio 1

---
+++ 1-visita-na-feira/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _1_visita_na_feira
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, x;
+            string[] values = Console.ReadLine().Split(' ');
+            a = Convert.ToInt32(values[0]);
+            b = Convert.ToInt32(values[1]);
+
+            x = a + b;
+
+            Console.WriteLine("X = {0}", x);
+        }
+    }
+}
