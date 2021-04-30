e17cd41a3d0e0aa018e193298eb1a2dcbdb27135
Filipe Braga <lipebraga16@hotmail.com>
2021-04-29 23:08:04 -0300

    Resolvendo Desafio 3

---
+++ 3-validacao-de-nota/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _3_validacao_de_nota
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double nota;
+            int qtdIgual = 0;
+            double soma = 0;
+            while (qtdIgual != 2)
+            {
+                nota = Convert.ToDouble(Console.ReadLine());
+                if (nota>=0 && nota<= 10)
+                {
+                    soma+=nota;
+                    qtdIgual++;
+                }
+                else
+                {
+                    Console.WriteLine("nota invalida");
+                }
+                
+            }
+            Console.WriteLine("media = {0:0.00}", (soma/2));
+        }
+    }
+}
