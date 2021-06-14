f3c50160c64580f3d9788b76c4d66755aa9ba86b
Filipe Braga <lipebraga16@hotmail.com>
2021-06-14 19:46:22 -0300

    Aplicando Design Patterns na Prática com C#

---
+++ aplicando-design-patterns-pratica-csharp/src/Domain/Repository/IDetranVerificadorDebitos.cs
@@ -0,0 +1,11 @@
+﻿using System.Threading.Tasks;
+using DesignPatternSamples.Application.DTO;
+
+
+namespace DesignPatternSamples.Domain.Repository.Detran
+{
+    public interface IDetranVerificadorDebitos
+    {
+        Task<DebitoVeiculo> ConsultarDebitos(Veiculo veiculo);
+    }
+}
---
+++ aplicando-design-patterns-pratica-csharp/src/Infra.Repository.Detran/DetranVerificadorDebitosRepositoryCrawlerBase.cs
@@ -0,0 +1,20 @@
+﻿using DesignPatternSamples.Application.DTO;
+using DesignPatternSamples.Application.Repository;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace DesignPatternSamples.Infra.Repository.Detran
+{
+    public abstract class DetranVerificadorDebitosRepositoryCrawlerBase : IDetranVerificadorDebitosRepository
+    {
+        public async Task<IEnumerable<DebitoVeiculo>> ConsultarDebitos(Veiculo veiculo)
+        {
+            //Usa métodos menores, abtratos - Template Method
+            var html = await RealizarAcesso(veiculo);
+            return await PadronizarResultado(html);
+        }
+
+        protected abstract Task<string> RealizarAcesso(Veiculo veiculo);
+        protected abstract Task<IEnumerable<DebitoVeiculo>> PadronizarResultado(string html);
+    }
+}
