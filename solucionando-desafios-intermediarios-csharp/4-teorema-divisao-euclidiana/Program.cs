9d4726f4996a9d8e56943de718afb77c841a06e8
Filipe Braga <lipebraga16@hotmail.com>
2021-06-15 01:18:28 -0300

    Resolvendo Desafio 4

---
+++ solucionando-desafios-intermediarios-csharp/4-teorema-divisao-euclidiana/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _4_teorema_divisao_euclidiana
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] valores = Console.ReadLine().Split();
+            int a = int.Parse(valores[0]);
+            int b = int.Parse(valores[1]);
+            int q, r;
+
+            q = a / b;
+            r = a - b * q;
+            if (r<0){
+                r += Math.Abs(b);
+                q = (a - r) /b;
+            }
+            Console.WriteLine("{0} {1}", q, r);
+        }
+    }
+}
