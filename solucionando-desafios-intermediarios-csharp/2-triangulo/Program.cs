747a6fedb10ed7de1db43acfd91644169e870693
Filipe Braga <lipebraga16@hotmail.com>
2021-06-14 23:58:46 -0300

    Resolvendo Desafio 2

---
+++ solucionando-desafios-intermediarios-csharp/2-triangulo/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+
+namespace _2_triangulo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a, b, c;
+            string[] valor = Console.ReadLine().Split();
+            a = Convert.ToDouble(valor[0]);
+            b = Convert.ToDouble(valor[1]);
+            c = Convert.ToDouble(valor[2]);
+
+            if((Math.Abs(b - c) < a && a < b + c) && 
+                (Math.Abs(a - c) < b && b < a + c ) && 
+                (Math.Abs(b - a) < c && c < b + a   ))
+            {
+                Console.WriteLine("Perimetro = {0:0.0}", a + b + c);
+            }
+            else
+            {
+                Console.WriteLine("Area = {0:0.0}", ((a + b) * c) / 2);
+            }
+        }
+    }
+}
